3d5dcdada800e6f41fff5fad2d17f49f2d9353fb
Jan Stastny <jstastny@redhat.com>
2018-02-12 08:03:00 +0100

    query runner for each query

--- query_tester.py
+++ query_tester.py
@@ -6,11 +6,10 @@ from results_comparator import ResultsComparator, ComparationResult
 from os import listdir, makedirs
 from os.path import isfile, isdir, join, exists
 from xml.etree import ElementTree as etree
-
+from time import sleep
 
 class QueryTester(object):
     db_connection = None
-    runner = None
     query_dir = None
     results_dir = None
     expected_dir = None
@@ -19,7 +18,6 @@ class QueryTester(object):
     def __init__(self, db_connection, query_dir, results_dir, scenario_name,
                  expected_dir):
         self.db_connection = db_connection
-        self.runner = QueryRunner(self.db_connection)
         self.query_dir = query_dir
         self.results_dir = results_dir
         self.expected_dir = expected_dir
@@ -46,11 +44,15 @@ class QueryTester(object):
             if not exists(result_dirname):
                 makedirs(result_dirname)
             for query_tuple in reader.read(self.query_dir + "/" + filename):
-                results_container = self.runner.run(*query_tuple)
-                result = result and (results_container is not None)
-                ResultsWriter(results_container).export(
-                    result_dirname + "/" + query_set_name + "_" +
-                    query_tuple[0] + ".xml")
+                try:
+                    runner=QueryRunner(self.db_connection)
+                    results_container = runner.run(*query_tuple)
+                    result = result and (results_container is not None)
+                    ResultsWriter(results_container).export(
+                        result_dirname + "/" + query_set_name + "_" +
+                        query_tuple[0] + ".xml")
+                finally:
+                    if runner is not None: runner.destroy()
         return result
 
     def compare_results(self, filename_given=None):
@@ -70,19 +72,23 @@ class QueryTester(object):
             for query_tuple in reader.read(self.query_dir + "/" + filename):
                 num_queries += 1
                 try:
+                    runner=QueryRunner(self.db_connection)
                     expected_filename = self.expected_dir + "/" + query_set_full_name + "/" + query_set_name + "_" + query_tuple[0] + ".xml"
                     expected_loader = ResultsLoader(expected_filename)
                     expected_results = expected_loader.load()
-                    actual_results = self.runner.run(*query_tuple)
+                except (IOError) as e:
+                    num_errors += 1
+                    print "ERROR: Couldn't load expected results file " + expected_filename, e
+                else:
+                    actual_results = runner.run(*query_tuple)
                     comp_result = ResultsComparator().compare(
                         expected_results, actual_results)
                     if not comp_result.success:
                         num_errors += 1
                         self.report_failures(query_set_name, expected_results,
                                              actual_results, comp_result)
-                except (IOError) as e:
-                    num_errors += 1
-                    print "ERROR: Couldn't load expected results file " + expected_filename, e
+                finally:
+                    if runner is not None: runner.destroy()
             sc_num_queries += num_queries
             sc_num_errors += num_errors
         filepath_summary_totals = self.results_dir + "/Summary_totals.txt"
